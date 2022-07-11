c63957c6a70525407f5e9eece2c5ade6dcff72d8
Godwin254 <godwinbee15@gmail.com>
2022-07-11 03:13:52 +0300

    add SecurityGuard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './styles/components/App.scss'
 import Home from './components/Home'
 import Admin from './components/Admin'
+import SecurityGuard from './components/SecurityGuard';
 
 function App() {
   return (
     <div className="App">
      {/** <Home />*/}
-     <Admin />
+     {/*<Admin />*/}
+     <SecurityGuard />
     </div>
   );
 }
