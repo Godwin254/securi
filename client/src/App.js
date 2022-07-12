2b8cc42432a32870aa7d9d7dac34e3e45a60da9c
Godwin254 <godwinbee15@gmail.com>
2022-07-12 22:52:38 +0300

    modify code

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,12 @@ import './styles/components/App.scss'
 import Home from './components/Home'
 import Admin from './components/Admin'
 import SecurityGuard from './components/SecurityGuard';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     {/** <Home />*/}
-     {/*<Admin />*/}
-     <SecurityGuard />
+      <Home />
     </div>
   );
 }
