73f4e17336d08cfc8001e8952671bf1ead0a7db0
Godwin254 <godwinbee15@gmail.com>
2022-07-17 16:21:26 +0300

    add Navbar and Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './styles/components/App.scss'
 import Home from './components/Home'
-import Admin from './components/Admin'
+import AdminLogin from './components/AdminLogin'
 import SecurityGuard from './components/SecurityGuard';
 import Resident from './components/Resident'
 import { Route, Routes, Link } from "react-router-dom";
@@ -9,28 +9,15 @@ function App() {
   return (
     
     <div className="App">
-      {/*<NavBar />*/}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/security" element={<SecurityGuard />} />
         <Route path="/resident" element={<Resident />} />
       </Routes>
     </div>
   );
 }
-
-/*
-const NavBar = () => {
-
-  return (
-    <div>
-      <Link to="/home"> Home</Link>
-      <Link to="/admin"> Admin</Link>
-      <Link to="/security"> Security</Link>
-    </div>
-  )
-}*/
 
 
 export default App;
--- client/src/components/Resident.js
+++ client/src/components/Resident.js
@@ -8,11 +8,54 @@ function Resident() {
   return (
     <div>
       {
-        isUser ? <Login /> : <Register />
+        //isUser ? <Login /> : <Register />
       }
+      <ResidentDashboard />
     </div>
    
   )
+}
+
+//resident dashboard
+const ResidentDashboard = () => {
+
+  return (
+    <div className="resident-dash">
+      <Navbar username = "John Doe" />
+
+      <Footer />
+    </div>
+  );
+}
+
+//Navbar component
+const Navbar = ({username}) => {
+
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <h2>Securi</h2>
+      </div>
+      <div className='links'>
+        <Link to='/home'>Home</Link>
+        <Link to='/notifications'>Notifications</Link>
+        <div className='user-profile'>
+          <img src='img/' alt='User Profile' />
+          <span>{username}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+//footer component
+const Footer = () => {
+
+  return (
+    <div className='footer'>
+      <span>&copy; Copyright 2022 | brownscode</span>
+    </div>
+  );
 }
 
 export default Resident;
