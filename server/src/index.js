7e8af55e6898f32e7f85c37512859ad0cb30834e
Godwin254 <godwinbee15@gmail.com>
2022-07-21 14:25:25 +0300

    set an entry point

--- server/src/index.js
+++ server/src/index.js
@@ -4,4 +4,8 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("<h1>Dev server started</h1>")
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
