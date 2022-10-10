ba7395d4596cea666d6b0bd479b4a2c323a0e863
Godwin254 <godwinbee15@gmail.com>
2022-10-10 18:33:26 +0300

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 // entry point
 const express = require('express');
-const PORT = process.env.PORT || 4000;
-
+const PORT = process.env.PORT || 8000;
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config();
+
+
+//middlewares
+app.use(bodyParser.json());
+
+//import routes
+const adminRoutes = require('./routes/adminRoutes');
+
+//route config
+app.use('/api/admins', adminRoutes);
+
+//db config
 
 app.get('/', (req, res) => {
     res.send("<h1>Dev server started</h1>")
 })
 
+//status
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
