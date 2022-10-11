8856c20bc78d0941cd999f31633914acb8a0ee81
Godwin254 <godwinbee15@gmail.com>
2022-10-11 16:34:21 +0300

    refactor: modify entry files

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
+//app.js
 const express = require('express');
-const PORT = process.env.PORT || 8000;
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
 
-//test server
-app.get('/', (req, res) => {
+
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());    
+
+//import routes
+const adminRoutes = require('./routes/adminRoutes');
+
+//route config
+app.use('/api/admins', adminRoutes);
+
+
+
+//test server running
+app.get('/api', (req, res) => {
     res.send("<h1>Dev server started</h1>")
 });
 
-//status
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-//export app
 module.exports = app;
