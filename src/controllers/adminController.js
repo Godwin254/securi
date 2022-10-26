5e0b314c93c1d8145f1d592d00906e18be811f8b
Godwin254 <godwinbee15@gmail.com>
2022-10-26 15:10:24 +0300

    update: modify admin controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const adminService = require('../services/adminService');
+const jwt = require('jsonwebtoken');
 
 //get all admins
 const getAllAdmins = async (req, res) => {
@@ -26,15 +27,77 @@ const getAdmin = async (req, res) => {
 //create a new admin
 const createAdmin = async (req, res) => {
     try{
+        ///register admin
+        if(!req.body){
+            res.status(400).json({ message: "All inputs must be provided" });
+        }
+
+        //check if user already exists
+        const oldAdmin = await adminService.getAdminByEmail(req.body.email);
+
+        if(oldAdmin){
+            res.status(400).json({ message: "Admin already exists" });
+        }
         //hash password
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         const newAdmin = { 
-            email: req.body.email,
+            email: req.body.email.toLowerCase(),
             password: hashedPassword,
             role: "admin"
         }
+
+        //create admin
         const admin = await adminService.createAdmin(newAdmin);
+
+        //create tokens
+        const token = jwt.sign({ 
+            admin_id: admin._id,
+            email: req.body.email},
+            process.env.JWT_TOKEN_SECRET,
+            { expiresIn: '2h' }
+        );
+
+        //save admin tokens
+        admin.token = token;
+        //admin
         res.status(201).json(admin);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
+
+//login admin
+const loginAdmin = async (req, res) => {
+    try{
+        //login admin
+        if(!req.body){
+            res.status(400).json({ message: "All inputs must be provided" });
+        }
+
+        //check if user already exists
+        const admin = await adminService.getAdminByEmail(req.body.email);
+
+        if(!admin){
+            res.status(400).json({ message: "Admin does not exist" });
+        }
+
+        if (admin && (await bcrypt.compare(req.body.password, admin.password))){
+            //create tokens
+            const token = jwt.sign({
+                admin_id: admin._id,
+                email: req.body.email},
+                process.env.JWT_TOKEN_SECRET,
+                { expiresIn: '2h' }
+            );
+
+            //save admin tokens
+            admin.token = token;
+            //admin
+            res.status(200).json(admin);
+        }
+
+        res.status(400).json({ message: "Invalid credentials" });
     }catch(err){
         res.status(500).json({message: err.message});
     }
@@ -67,6 +130,7 @@ module.exports = {
     getAllAdmins,
     getAdmin,
     createAdmin,
+    loginAdmin,
     updateAdmin,
     deleteAdmin
 }
