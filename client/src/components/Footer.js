c5cfdf97051562fbc864c0191806fb250c9270c7
Godwin254 <godwinbee15@gmail.com>
2022-10-27 12:50:10 +0300

    feat: modify client

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import '../styles/components/Footer.scss'
+import {Link} from 'react-router-dom'
 
 //footer component
 const Footer = () => {
 
+    const year = new Date().getFullYear()
+
     return (
-        <div className='footer'>
-            <span>&copy; Copyright 2022 | brownscode</span>
+        <div className='Footer'>
+            <h1>SECURI</h1>
+            <p>© {year} SECURI. All rights reserved</p>
+            <div className='Footer__links'>
+                <Link to='/'>Home</Link>
+                <Link to='https://godwin254.github.io/portfolio-/'>Portfolio</Link>
+                <Link to='https://github.com/Godwin254'>Github</Link>
+            </div>   
         </div>
     );
 }
