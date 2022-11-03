e807df11a0fdaf85d5ffc66bbc06b8b26d5aad44
Godwin254 <godwinbee15@gmail.com>
2022-11-03 17:54:01 +0300

    feat: add auth for tokens on frontend

---
+++ client/src/auth/AdminProtectedRoutes.js
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import { useUser } from './useUser'
+
+
+export const AdminProtectedRoutes = ({ children }) => {
+    const admin = useUser();
+
+    return  admin ? <Outlet /> : <Navigate to='/admin-login' />;
+}
